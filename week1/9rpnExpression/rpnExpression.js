c6ecf31ce8400fd936e5d9c713e18a654bfbd022
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2024-12-28 18:42:56 +0300

    rpn expression

--- week1/9rpnExpression/rpnExpression.js
+++ week1/9rpnExpression/rpnExpression.js
@@ -3,7 +3,6 @@ Description: Reverse Polish Notation (RPN) is a mathematical notation in which e
 Your task is to implement a simple RPN calculator using the reduceRight() method. 
 The input will be an array of strings, where each string is either a number or an operator (+, -, *, /).
 
-
 */
 
 let rpnExpression = ['3', '4', '+', '2', '*', '7', '/'];
