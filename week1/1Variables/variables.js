113dfb63435726d20dae11aa4d63fa83d40bac54
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2024-12-28 18:14:36 +0300

    variables in java script

--- week1/1Variables/variables.js
+++ week1/1Variables/variables.js
@@ -1,7 +1,17 @@
 let name = "Haile";
 
-var speech = "I love Marti to much";
+var speech = " loves Marti to much";
+const sex = 'male';
+const pronoun = '' ;
+
+
+if(sex == 'male'){
+    pronoun = "he";
+}
+else{
+    pronoun = "she";
+}
 
 const PI = 3.14; // not allow to redeclare the variable
 
-console.log(`${name} says , ${speech}`)
+console.log(`${name} says  that , ${pronoun} ${speech}`)
